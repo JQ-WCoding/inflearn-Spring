95b7cd3d5734491aa7388fdf8ce70658905c7d9c
Jun_Q <junlee36520620@gmail.com>
2021-12-04 20:57:31 +0900

    mvc와 템플릿 엔진

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -16,5 +17,14 @@ public class HelloController {
         return "hello";
 
         // springboot dev tool 라이브러리를 통해 쉽게 사용하는 법 찾아보기
+    }
+
+    @GetMapping ( "hello-mvc" )
+    // 파라미터 name 을 요구 -> required = false 를하면 필요없다는 뜻
+    public String helloMvc(@RequestParam ( name = "name", required = false ) String name, Model model) {
+        // "name" key , 문자열 name 이 value
+        model.addAttribute( "name", name );
+        // hello-template 파일로 이동 -> template 엔진을 통해 변환 과정을 거친 html 파일을 넘겨준다
+        return "hello-template";
     }
 }
