2a8da4776498f657b8bdc6ce372372f9fcdeb422
Jun_Q <junlee36520620@gmail.com>
2021-12-04 22:07:09 +0900

    api

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -26,5 +27,39 @@ public class HelloController {
         model.addAttribute( "name", name );
         // hello-template 파일로 이동 -> template 엔진을 통해 변환 과정을 거친 html 파일을 넘겨준다
         return "hello-template";
+    }
+
+    @GetMapping ( "hello-string" )
+    // http 에서 header 부분과 body 부분 중 응답 body 부분에 직접 넣겠다는 의미
+    @ResponseBody
+    public String helloString(@RequestParam ( "name" ) String name) {
+        return "hello " + name; // "hello ~~~ " 직접 그대로 데이터만 넘겨주는 형식
+    }
+
+    @GetMapping ( "hello-api" )
+    // JSON 반환이 기본
+    @ResponseBody
+    public Hello helloApi(@RequestParam ( "name" ) String name) {
+        Hello hello = new Hello();
+        hello.setName( name );
+
+        // JSON 방식으로 파싱해서 객체를 값으로 넘겨줌
+        // 객체가 오면 JSON 방식을 표준으로 변환하여 값을 넘겨준다
+        // name key 와 ?name = abc 와 같이 abc 가 value 로 넘겨짐
+        return hello;
+        // 이후 http message converter 에서 JsonConverter 혹은 StringConverter 등을 확인하고 알맞게 조정하여 보낸다
+        // 기본 객체처리 : MappingJackson2HttpMessageConverter
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
