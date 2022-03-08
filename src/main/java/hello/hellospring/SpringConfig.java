0603f4b21a54165cf6813936ec7be4f9e06cf481
Jun_Q <junlee36520620@gmail.com>
2022-03-08 21:14:23 +0900

    OCP 원칙

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -40,6 +40,8 @@ public class SpringConfig {
 
 
     /* 의존성 주입을 외부에서 간단하게 붙였다 때는 형식으로 진행하여 해당 @Bean 만으로 간단하게 레포지토리를 변경가능하도록 한다 */
+    // 해당 bean 은 사용자들이 객체를 바꾸면서 선언해주어야한다 -> 결국 클라이언트가 계속해서 변경을 해주어야한다
+    // 따라서, OCP 개방폐쇄 원칙에 문제가 생긴다 다형성을 이용해 객체지향적으로 만들었지만 결국 해치게 되는 아이러니한 현상이 발생
     @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
