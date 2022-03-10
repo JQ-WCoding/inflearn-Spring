fa50b54220ff7fe1202e13919647f669135238d8
Jun_Q <junlee36520620@gmail.com>
2022-03-10 23:56:55 +0900

    인터페이스 구현

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 
 public interface MemberRepository {
+    // 사용자 저장, 찾기 등을 위해 미리 구현한 인터페이스로 구현한 메소드 -> 지정만 해두고 나머지 구현은 직접
+    // MemoryMemberRepository 에서 구현한다 Optional 의 사용이유
     Member save(Member member);
 
     Optional<Member> findById(Long id);
