b140d1b582684c014c1ca43501ed605576f228e2
Jun_Q <junlee36520620@gmail.com>
2022-03-11 18:23:03 +0900

    Optional 클래스

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -20,12 +20,14 @@ public class MemoryMemberRepository implements MemberRepository {
     @Override
     public Optional<Member> findById(Long id) {
         // null 일 경우를 대비해 Optional 로 감싸서 보낸다
+        // null 인지 value of 를 이용해 값이 있는지 확인하고 반환한다
         return Optional.ofNullable( store.get( id ) );
     }
 
     @Override
     public Optional<Member> findByName(String name) {
         // 끝까지 하나라도 찾으면 반환하는 방법
+        // findAny() 메소드를 통해 하나라도 찾게 되면 반환한다 -> name 으로 찾기 때문에 우선적인 조회가 필요함
         return store.values().stream().filter( member -> member.getName().equals( name ) ).findAny();
     }
 
